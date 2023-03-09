d87e31da22e6b671fefe039c34d96fddb0915a84
Moritz Diedrich <mrtz.ddrch@gmx.de>
2023-03-09 17:13:56 +0100

    optimized bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -4,12 +4,17 @@
 let arr = [1,3,6,8,15,24,34,35,47,59,5,3,6,8,12,13]
 function bubbleSort(arr){
   for(let i = arr.length - 1; i >= 0; i--){
+    let noSwap = true;
     for(let j = 0; j < i; j++){
       if(arr[j] > arr[j+1]){
-        let temp = arr[j];
+        let arrj = arr[j];
         arr[j] = arr[j+1];
-        arr[j+1] = temp;
+        arr[j+1] = arrj;
+        noSwap = false;
       }
+    }
+    if(noSwap){
+      break;
     }
   }
   return arr;
