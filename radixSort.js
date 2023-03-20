29c72419cfeee31957fce6106aaccc7d90930aa7
Moritz Diedrich <mrtz.ddrch@gmx.de>
2023-03-20 13:52:37 +0100

    Preparing for Radix Sort with Helper Methods

---
+++ radixSort.js
@@ -0,0 +1,20 @@
+// Preparing some Helper Methods for Radix Sort
+// These are taken from Stack Overflow
+
+function getDigit(num, i){
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+
+function digitCount(num){
+  if(num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(arr){
+  let maxDigits = 0;
+  for(let i = 0; i < arr.length; i++){
+    maxDigits = Math.max(maxDigits, digitCount(arr[i]));
+  }
+  return maxDigits;
+}
+
