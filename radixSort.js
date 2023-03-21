c4f341610705e7cb5a7e9b9a5c20d8114686a89f
Moritz Diedrich <mrtz.ddrch@gmx.de>
2023-03-21 11:52:23 +0100

    Radix Sort v1

--- radixSort.js
+++ radixSort.js
@@ -1,20 +1,34 @@
-// Preparing some Helper Methods for Radix Sort
-// These are taken from Stack Overflow
+// Helper functions rearranged in the order they are called
+// I combined and simplified two of the functions as it seemed unnecessarily complex to me otherwise
+// I have to admit that I am not a fan of this one
+
+function maxDigits(arr){
+  let maxDigits = 0;
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === 0) continue;
+    let temp = Math.floor(Math.log10(Math.abs(arr[i]))) + 1;
+    if(temp > maxDigits) maxDigits = temp;
+  }
+  return maxDigits;
+}
 
 function getDigit(num, i){
   return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
 
-function digitCount(num){
-  if(num === 0) return 1;
-  return Math.floor(Math.log10(Math.abs(num))) + 1;
-}
-
-function mostDigits(arr){
-  let maxDigits = 0;
-  for(let i = 0; i < arr.length; i++){
-    maxDigits = Math.max(maxDigits, digitCount(arr[i]));
+function radixSort(arr){
+  var maxDigitCount = maxDigits(arr);
+  for(var k = 0; k <= maxDigitCount; k++){
+    var digitBuckets = Array.from({length: 10}, () => []);
+    for(var i = 0; i < arr.length; i++){
+      let digit = getDigit(arr[i], k);
+      digitBuckets[digit].push(arr[i]);
+    }
+    arr = [].concat(...digitBuckets);
   }
-  return maxDigits;
+  return arr;
 }
 
+var numbers = [12,53,765,2345,86,35,98,46,98576,295,84];
+
+console.log(radixSort(numbers));
