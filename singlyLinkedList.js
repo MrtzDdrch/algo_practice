ea6ad1110746c3733e2e2eb32cea8d61aa57cd66
Moritz Diedrich <mrtz.ddrch@gmx.de>
2023-03-24 14:46:18 +0100

    Singly Linked List, part 1

---
+++ singlyLinkedList.js
@@ -0,0 +1,28 @@
+// Working on a Singly Linked List. Unfinished, work in progress...
+
+class Node{
+  constructor(val){
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList{
+  constructor(){
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+  push(val){
+    var newNode = new Node(val);
+    if(!this.head){
+      this.head = newNode;
+      this.tail = newNode;
+    }else{
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
