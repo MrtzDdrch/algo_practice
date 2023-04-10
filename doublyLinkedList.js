17313df4831eb687aba03c368b880305d6387cb4
Moritz Diedrich <mrtz.ddrch@gmx.de>
2023-04-10 20:18:20 +0200

    Doubly Linked List pop() method added

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -14,6 +14,18 @@ class DoublyLinkedList{
     this.tail = null;
     this.length = 0;
   }
+  logAll(){
+    if(this.length > 0){
+      var current = this.head;
+      var count = this.length;
+      while(count > 0){
+        console.log(current.val);
+        current = current.next;
+        count--;
+      }
+    }
+    return undefined;
+  }
   // Push new node to the end of the list
   push(val){
     var newNode = new Node(val);
@@ -28,11 +40,26 @@ class DoublyLinkedList{
     this.length++;
     return this;
   }
+  // Remove node from end of list
   pop(){
-
+    if(!this.tail) return undefined;
+    var poppedNode = this.tail;
+    if(poppedNode.prev){
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }else{
+      this.head = null;
+      this.tail = null;
+    }
+    this.length--;
+    return poppedNode.val;
   }
   shift(){
 
+  }
+  unshift(){
+    
   }
   get(){
 
