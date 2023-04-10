05cb4285f3f07c8d6b054fbf062f30681918404d
Moritz Diedrich <mrtz.ddrch@gmx.de>
2023-04-10 12:27:18 +0200

    Doubly Linked List preparations

---
+++ doublyLinkedList.js
@@ -0,0 +1,38 @@
+// Another linked list. Oh joy. 
+
+class Node{
+  constructor(val){
+    this.val = val;
+    this.next = null;
+    this.previous = null;
+  }
+}
+
+class DoublyLinkedList{
+  constructor(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(){
+
+  }
+  pop(){
+
+  }
+  shift(){
+
+  }
+  get(){
+
+  }
+  set(){
+
+  }
+  insert(){
+
+  }
+  remove(){
+    
+  }
+}
