0405ef96532d3c70ac1ce6e117b150bb6f768396
Moritz Diedrich <mrtz.ddrch@gmx.de>
2023-04-18 23:49:28 +0200

    Binary Search Tree find() method added

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -18,29 +18,46 @@ class BinarySearchTree{
     if(!this.root){
       this.root = newNode;
       return this;
-    }else{
-      var current = this.root;
-      while(true){
-        // I would prefer a frequency counter here but for now I am just working on the basics and it depends on the use case anyway 
-        if(val === current.val) return undefined;
-        if(val < current.val){
-          if(current.left === null){
-            current.left = newNode;
-            return this;
-          }
-          current = current.left;
-        }else if(val > current.val){
-          if(current.right === null){
-            current.right = newNode;
-            return this;
-          }
-          current = current.right;
+    }
+    var current = this.root;
+    while(true){
+      // I would prefer a frequency counter here but for now I am just working on the basics and it depends on the use case anyway 
+      if(val === current.val) return undefined;
+      if(val < current.val){
+        if(current.left === null){
+          current.left = newNode;
+          return this;
         }
+        current = current.left;
+      }else if(val > current.val){
+        if(current.right === null){
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
       }
-    }    
+    }
+  }
+  find(val){
+    if(typeof(val) !== 'number') return undefined;
+    if(!this.root) return false;
+    var current = this.root;
+    while(true){
+      if(val === current.val) return current;
+      if(val < current.val){
+        if(current = current.left){
+          continue;
+        }
+        return false;
+      }
+      if(val > current.val){
+        if(current = current.right){
+          continue;
+        }
+        return false;
+      }
+    }
   }
 }
 
 var tree = new BinarySearchTree();
-
-
