6f2a4b85af3a96ddb9566cde3112407c6d581798
Moritz Diedrich <mrtz.ddrch@gmx.de>
2023-04-18 23:17:13 +0200

    Binary Search Tree insert() method added

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,3 +1,5 @@
+// Exploring the concept of a binary search tree because life can be fun
+
 class Node{
   constructor(val){
     this.val = val;
@@ -10,6 +12,35 @@ class BinarySearchTree{
   constructor(){
     this.root = null;
   }
-
+  insert(val){
+    if(typeof(val) !== 'number') return undefined;
+    var newNode = new Node(val);
+    if(!this.root){
+      this.root = newNode;
+      return this;
+    }else{
+      var current = this.root;
+      while(true){
+        // I would prefer a frequency counter here but for now I am just working on the basics and it depends on the use case anyway 
+        if(val === current.val) return undefined;
+        if(val < current.val){
+          if(current.left === null){
+            current.left = newNode;
+            return this;
+          }
+          current = current.left;
+        }else if(val > current.val){
+          if(current.right === null){
+            current.right = newNode;
+            return this;
+          }
+          current = current.right;
+        }
+      }
+    }    
+  }
 }
+
+var tree = new BinarySearchTree();
+
 
