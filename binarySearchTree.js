33a2f4f4ac3c9a4286dfd40d22fb98fdb3a9f395
Moritz Diedrich <mrtz.ddrch@gmx.de>
2023-04-18 22:50:45 +0200

    Starting work on Binary Search Tree

---
+++ binarySearchTree.js
@@ -0,0 +1,15 @@
+class Node{
+  constructor(val){
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree{
+  constructor(){
+    this.root = null;
+  }
+
+}
+
